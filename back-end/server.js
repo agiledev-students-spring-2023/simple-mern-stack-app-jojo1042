da47e27a021ecf56f2f5207f6ea049ecaab6a0d5
jojo1042 <jojo.yang1042@gmail.com>
2023-02-14 23:56:17 -0500

    attempts

--- back-end/server.js
+++ back-end/server.js
@@ -4,8 +4,12 @@
 const server = require('./app')
 require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
 
+const app = express()
+
 // which port to listen for HTTP(S) requests
 const port = process.env.PORT || 3000
+
+app.use('./front-end', express.static('images'))
 
 // call a function to start listening to the port
 const listener = server.listen(port, function () {
