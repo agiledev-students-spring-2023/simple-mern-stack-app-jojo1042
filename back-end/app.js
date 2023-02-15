a62117b2be886e2dbd64663f095b020874544ec8
jojo1042 <jojo.yang1042@gmail.com>
2023-02-15 11:48:53 -0500

    added an aboutus model in the backend

--- back-end/app.js
+++ back-end/app.js
@@ -21,6 +21,7 @@ mongoose
 // load the dataabase models we want to deal with
 const { Message } = require('./models/Message')
 const { User } = require('./models/User')
+const { AboutUs } = require('./models/AboutUs')
 
 // a route to handle fetching all messages
 app.get('/messages', async (req, res) => {
@@ -60,24 +61,20 @@ app.get('/messages/:messageId', async (req, res) => {
 
 // route for about us
 app.get('/aboutus', async (req, res) => {
-  // try {
-
-  //   res.json({
-  //   name: "JoJo Yang",
-  //   text: "Hello! My name is JoJo, and this is my last semester at NYU. I'm a double major in Linguistics and CS, and my interests are mostly in machine learning and AI, though I'm working in IT in the fall after I graduate. Last year I took a natural language processing course that was very challenging but super interesting, to translate English words to their IPA phonetic counterparts. Spoiler alert: not really, it turns out the inconsistencies between English spelling and pronunciation was a little too much for our model to handle. But I'd love to talk more about it! My family is originally from Sichuan, China, but I grew up in the suburbs of Boston, MA. I played a couple of instruments growing up, one that is a traditional Chinese instrument called the hammered dulcimer, as well as the clarinet. I'm also currently learning to play the guitar. My other hobbies include cooking, sleeping, listening to music every minute I'm awake (usually alternative music though who even knows what that genre is really), fencing (though I haven't been able to do that in a while), looking at photos of dogs, and playing Dungeons and Dragons with my friends on the weekend. Hmm... anything else... my favorite dog breed is the corgi, and I had a pet hamster named Athena who passed away last month :( I follow video games and movies/TV shows pretty closely, so naturally my current favorite series is the Last of Us. You should definitely go watch it if you haven't already. Severance and the White Lotus are also both excellent shows. Okay I think that is enough about me, see ya! (and go watch the Last of Us"
-  // })
-  // }
-  // catch (err) {
-  //   console.error(err)
-  //   res.status(400).json({
-  //     error: err,
-  //     status: 'failed to get'
-  //   })
-  // }
-  return res.json({
-    name: "JoJo Yang",
-    text: "Hello! My name is JoJo, and this is my last semester at NYU. I'm a double major in Linguistics and CS, and my interests are mostly in machine learning and AI, though I'm working in IT in the fall after I graduate. Last year I took a natural language processing course that was very challenging but super interesting, to translate English words to their IPA phonetic counterparts. Spoiler alert: not really, it turns out the inconsistencies between English spelling and pronunciation was a little too much for our model to handle. But I'd love to talk more about it! My family is originally from Sichuan, China, but I grew up in the suburbs of Boston, MA. I played a couple of instruments growing up, one that is a traditional Chinese instrument called the hammered dulcimer, as well as the clarinet. I'm also currently learning to play the guitar. My other hobbies include cooking, sleeping, listening to music every minute I'm awake (usually alternative music though who even knows what that genre is really), fencing (though I haven't been able to do that in a while), looking at photos of dogs, and playing Dungeons and Dragons with my friends on the weekend. Hmm... anything else... my favorite dog breed is the corgi, and I had a pet hamster named Athena who passed away last month :( I follow video games and movies/TV shows pretty closely, so naturally my current favorite series is the Last of Us. You should definitely go watch it if you haven't already. Severance and the White Lotus are also both excellent shows. Okay I think that is enough about me, see ya! (and go watch the Last of Us"
-  })
+  try {
+    const aboutus = await AboutUs.find({})
+    res.json({
+      aboutus: aboutus,
+      status: 'all good',
+    })
+  }
+  catch (err) {
+    console.error(err)
+    res.status(400).json({
+      error: err,
+      status: 'failed to get'
+    })
+  }
   
 })
 
